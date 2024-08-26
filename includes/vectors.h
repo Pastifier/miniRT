47982198dede70f487abee8ebc0624977948dfec
Emran Binjamaan <therealemran.design@gmail.com>
2024-08-27 01:38:10 +0400

    raytraced a circle boiiiii

--- includes/vectors.h
+++ includes/vectors.h
@@ -6,7 +6,7 @@
 /*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 10:46:58 by ebinjama          #+#    #+#             */
-/*   Updated: 2024/08/26 12:53:08 by ebinjama         ###   ########.fr       */
+/*   Updated: 2024/08/27 01:30:31 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,7 @@ t_vector3	vec3_scaleby(t_vector3 a, double f);
 double		vec3_magnitude(t_vector3 a);
 double		vec3_magnitude_sqrd(t_vector3 a);
 t_vector3	vec3_cross(t_vector3 u, t_vector3 v);
+double	vec3_dot(t_vector3 u, t_vector3 v);
 t_vector3	vec3_normal(t_vector3 v);
 
 #endif // !__VECTORS_H
--- srcs/colors.c
+++ srcs/colors.c
@@ -6,7 +6,7 @@
 /*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 12:00:07 by ebinjama          #+#    #+#             */
-/*   Updated: 2024/08/27 01:18:09 by ebinjama         ###   ########.fr       */
+/*   Updated: 2024/08/27 01:36:03 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include "vectors.h"
 #include "ray.h"
 #include "log.h"
+#include <stdbool.h>
 
 uint32_t	assign_color(uint8_t r,uint8_t g, uint8_t b)
 {
@@ -39,20 +40,41 @@ void	pixel_put(t_data *data, t_vector2 point, int color)
 }
 
 /*
-color ray_color(const ray& r) {
-    vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5*(unit_direction.y() + 1.0);
-    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
 }
 */
+
+bool	hit_sphere(t_vector3 center, double radius, t_ray ray)
+{
+	t_vector3	distance_to_center;
+	double		a;
+	double		b;
+	double		c;
+	double		discriminant;
+
+	distance_to_center = vec3_subtract(center, ray.origin);
+	a = vec3_dot(ray.dir, ray.dir);
+	b = -2.0 * vec3_dot(ray.dir, distance_to_center);
+	c = vec3_dot(distance_to_center, distance_to_center) - (radius * radius);
+	discriminant = (b * b) - (4 * a * c);
+	return (discriminant >= 0);
+}
 
 uint32_t	ray_color(t_ray ray)
 {
 	double		a;
 	t_vector3	normal;
 
+	if (hit_sphere((t_vector3){0, 0, -1}, 0.5, ray))
+		return (assign_color(0xFF, 0x0, 0x0));
 	normal = vec3_normal(ray.dir);
 	a = 0.5 * (normal.y + 1.0);
 	return (assign_color(0xFF * (1 - a) , 0xFF * (1 - a), 0xFF * (1 - a))
-			+ assign_color(0x0F * a, 0xF0 * a, 0xFF * a));
+			+ assign_color(0xFF * 0.5 * a, 0xFF * 0.7 * a, 0xFF * a));
 }
