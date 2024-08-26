4d4716b322d341f002dcc76c993ef3e952209f7d
MOMO24185 <mahmoud200012345@gmail.com>
2024-08-26 23:08:53 +0400

    Added colored shading for sphere based on its normal vectors, and simplified hit_sphere function

--- srcs/colors.c
+++ srcs/colors.c
@@ -12,6 +12,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include <math.h>
 #include "minirt.h"
 #include "vectors.h"
 #include "ray.h"
@@ -40,30 +41,42 @@ void	pixel_put(t_data *data, t_vector2 point, int color)
 }
 
 //Temp for testing spheres
-static bool	hit_sphere(t_vector3 center, double radius, t_ray *ray)
+static double	hit_sphere(t_vector3 center, double radius, t_ray *ray)
 {
 	t_vector3	oc;
 	double		a;
-	double		b;
+	double		h;
 	double		c;
 	double		discriminant;
 
 	oc = vec3_subtract(center, ray->origin);
-	a = vec3_dot(ray->dir, ray->dir);
-	b = -2.0 * vec3_dot(ray->dir, oc);
-	c = vec3_dot(oc, oc) - (radius * radius);
-	discriminant = b * b - 4 * a * c;
-	return (discriminant >= 0);
+	a = vec3_magnitude_sqrd(ray->dir);
+	h = vec3_dot(ray->dir, oc);
+	c = vec3_magnitude_sqrd(oc) - (radius * radius);
+	discriminant = h * h - a * c;
+	
+	if (discriminant < 0)
+		return (-1.0);
+	else
+		return ((h - sqrt(discriminant)) / a);
 }
 
 uint32_t	ray_color(t_ray *ray)
 {
 	t_vector3	unit_dir;
+	t_vector3	norm;
 	double		t;
 
-	//Temp for testing spheres
-	if (hit_sphere((t_vector3){0, 0, -1}, 0.5, ray))
-		return (assign_color(255, 0, 0));
+	//Testing spheres
+	t = hit_sphere((t_vector3){0, 0, -1}, 0.5, ray);
+	if (t > 0.0)
+	{
+		norm = vec3_normal(vec3_subtract(vec3_at_tip(*ray, t), (t_vector3){0, 0, -1}));
+		// Scale the normal vector components to [0, 1] range
+		return (assign_color(255 * (0.5 * (norm.x + 1)), 
+							 255 * (0.5 * (norm.y + 1)), 
+							 255 * (0.5 * (norm.z + 1))));
+	}
 
 	unit_dir = vec3_normal(ray->dir);
 	t = 0.5 * (unit_dir.y + 1.0);
--- srcs/main.c
+++ srcs/main.c
@@ -22,14 +22,14 @@ int	main(int argc, char *argv[])
 	init_program(argc, argv, &context);
 	t_vector3 viewport_upper_left = vec3_subtract(vec3_subtract(context.camera.origin, (t_vector3){0, 0, context.camera.focal_length}), vec3_subtract(vec3_scaleby(context.camera.viewport_u, 0.5), vec3_scaleby(context.camera.viewport_v, 0.5)));
 	vec3_print(viewport_upper_left);
-	t_vector3 pixel00_location = vec3_add(viewport_upper_left, vec3_scaleby(vec3_add(context.camera.pixel_delta_u, context.camera.pixel_delta_v), 0.5));
-	pixel00_location.y = -pixel00_location.y;
-	vec3_print(pixel00_location);
+	t_vector3 pixel_00_location = vec3_add(viewport_upper_left, vec3_scaleby(vec3_add(context.camera.pixel_delta_u, context.camera.pixel_delta_v), 0.5));
+	pixel_00_location.y = -pixel_00_location.y;
+	vec3_print(pixel_00_location);
 	for(int y = 0; y < WIN_HEIGHT; y++)
 	{
 		for(int x = 0; x < WIN_WIDTH; x++)
 		{
-			t_vector3 pixel_center = vec3_add(pixel00_location, vec3_add(vec3_scaleby(context.camera.pixel_delta_u, x), vec3_scaleby(context.camera.pixel_delta_v, y)));
+			t_vector3 pixel_center = vec3_add(pixel_00_location, vec3_add(vec3_scaleby(context.camera.pixel_delta_u, x), vec3_scaleby(context.camera.pixel_delta_v, y)));
 			t_ray ray = (t_ray){context.camera.origin, vec3_subtract(pixel_center, context.camera.origin)};
 			uint32_t color = ray_color(&ray);
 			pixel_put(&context.img, (t_vector2){x, y}, color);
