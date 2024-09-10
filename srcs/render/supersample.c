62ad939e2d77775203d8d9715866601045902268
Emran Binjamaan <therealemran.design@gmail.com>
2024-09-10 16:14:10 +0400

    Just changed '5' to '5.0' for my own ease-of-mind (it gets implicitly type-casted into float-division thanks to the '1.0' either way).

--- srcs/render/supersample.c
+++ srcs/render/supersample.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   supersample.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: melshafi <melshafi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 14:48:07 by melshafi          #+#    #+#             */
-/*   Updated: 2024/09/09 15:48:41 by melshafi         ###   ########.fr       */
+/*   Updated: 2024/09/10 16:03:32 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ t_color	super_sample_pix(t_thread_data *data, double x, double y)
 			cadd(&avg_color, &avg_color, &color);
 		}
 	}
-	cscale(&avg_color, &avg_color, 1.0 / 5);
+	cscale(&avg_color, &avg_color, 1.0 / 5.0);
 	cclamp(&avg_color);
 	put_pixel(&data->context->canvas, x, y, avg_color);
 	return (avg_color);
