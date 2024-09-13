31a57db2c7597efd6617422e5a4818a7d5996d57
Emran Binjamaan <therealemran.design@gmail.com>
2024-09-13 08:28:05 +0400

    Further optimised mat4x4_cross() by adding SIMD support

--- srcs/math/matrix4_4.c
+++ srcs/math/matrix4_4.c
@@ -17,26 +17,48 @@
 
 t_mat4x4 mat4x4_cross(t_mat4x4 *m1, t_mat4x4 *m2)
 {
-	int r;
-	t_double4 col;
-	t_mat4x4 result;
-	t_double4 *row;
+	t_mat4x4	result;
+	__m256d		row;
+	__m256d		col[4];
+	int			r;
 
+	col[0] = avx_extract_column(m2, 0);
+	col[1] = avx_extract_column(m2, 1);
+	col[2] = avx_extract_column(m2, 2);
+	col[3] = avx_extract_column(m2, 3);
 	r = -1;
-	row = &(m1->r1);
 	while (++r < 4)
 	{
-		col = extract_column(m2, 0);
-		result.m[r][0] = vdot(&row[r], &col);
-		col = extract_column(m2, 1);
-		result.m[r][1] = vdot(&row[r], &col);
-		col = extract_column(m2, 2);
-		result.m[r][2] = vdot(&row[r], &col);
-		col = extract_column(m2, 3);
-		result.m[r][3] = vdot(&row[r], &col);
+		row = _mm256_set_pd(m1->m[r][3], m1->m[r][2], m1->m[r][1], m1->m[r][0]);
+		result.m[r][0] = avx_dot_product(row, col[0]);
+		result.m[r][1] = avx_dot_product(row, col[1]);
+		result.m[r][2] = avx_dot_product(row, col[2]);
+		result.m[r][3] = avx_dot_product(row, col[3]);
 	}
 	return (result);
 }
+//t_mat4x4 mat4x4_cross(t_mat4x4 *m1, t_mat4x4 *m2)
+//{
+//	int r;
+//	t_double4 col;
+//	t_mat4x4 result;
+//	t_double4 *row;
+
+//	r = -1;
+//	row = &(m1->r1);
+//	while (++r < 4)
+//	{
+//		col = extract_column(m2, 0);
+//		result.m[r][0] = vdot(&row[r], &col);
+//		col = extract_column(m2, 1);
+//		result.m[r][1] = vdot(&row[r], &col);
+//		col = extract_column(m2, 2);
+//		result.m[r][2] = vdot(&row[r], &col);
+//		col = extract_column(m2, 3);
+//		result.m[r][3] = vdot(&row[r], &col);
+//	}
+//	return (result);
+//}
 
 bool	mat4x4_eq(t_mat4x4 *m1, t_mat4x4 *m2)
 {
@@ -73,11 +95,13 @@ bool	mat4x4_eq(t_mat4x4 *m1, t_mat4x4 *m2)
 t_double4	mat4x4_cross_vec(t_mat4x4 *m, t_double4 *v)
 {
 	t_double4	result;
+	__m256d		simd_v;
 
-	result.x = vdot(&m->r1, v);
-	result.y = vdot(&m->r2, v);
-	result.z = vdot(&m->r3, v);
-	result.w = vdot(&m->r4, v);
+	simd_v = simdify_double4(v);
+	result.x = avx_dot_product(simdify_double4(&m->r1), simd_v);
+	result.y = avx_dot_product(simdify_double4(&m->r2), simd_v);
+	result.z = avx_dot_product(simdify_double4(&m->r3), simd_v);
+	result.w = avx_dot_product(simdify_double4(&m->r4), simd_v);
 	return (result);
 }
 
