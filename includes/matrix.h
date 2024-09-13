f094b734d2b4a3aa37ec7cb38d123422cc1c5925
Emran Binjamaan <therealemran.design@gmail.com>
2024-09-13 08:25:46 +0400

    Added simd support with inline compiler-suggestion

--- includes/matrix.h
+++ includes/matrix.h
@@ -6,7 +6,7 @@
 /*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 01:54:47 by ebinjama          #+#    #+#             */
-/*   Updated: 2024/09/12 21:37:05 by ebinjama         ###   ########.fr       */
+/*   Updated: 2024/09/13 05:41:44 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 # include "rtmath.h"
 # include <stdbool.h>
+# include <immintrin.h>
+# include <assert.h>
 
 //typedef struct s_mat4x4
 //{
@@ -54,7 +56,7 @@ typedef union u_mat2x2
 	};
 }	t_mat2x2;
 
-static inline t_double4 extract_column(t_mat4x4 *m, short c)
+static inline t_double4	extract_column(t_mat4x4 *m, short c)
 {
 	return ((t_double4)
 		{
@@ -63,6 +65,28 @@ static inline t_double4 extract_column(t_mat4x4 *m, short c)
 		.z = m->m[2][c],
 		.w = m->m[3][c]
 		});
+}
+
+static inline __m256d	avx_extract_column(t_mat4x4 *m, short c)
+{
+	assert(c >= 0 && c < 4);
+	return (_mm256_set_pd(m->m[3][c], m->m[2][c], m->m[1][c], m->m[0][c]));
+}
+
+static inline double	avx_dot_product(__m256d a, __m256d b)
+{
+	__m256d			temp;
+	double			result[4];
+	const __m256d	mul = _mm256_mul_pd(a, b);
+
+	temp = _mm256_hadd_pd(mul, mul);
+	_mm256_storeu_pd(result, temp);
+	return (result[0] + result[2]);
+}
+
+static inline __m256d	simdify_double4(t_double4 *v)
+{
+	return (_mm256_set_pd(v->v[3], v->v[2], v->v[1], v->v[0]));
 }
 
 t_double4	row4(double x, double y, double z, double w);
