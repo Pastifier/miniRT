0c7d223da550e65f3bc04822563dec78f1e70f61
Emran Binjamaan <therealemran.design@gmail.com>
2024-09-13 08:30:11 +0400

    Made use of a pre-cached inverse matrix for frame-by-frame inverse calculation as opposed to ray-by-ray (for each pixel)

--- srcs/objects/plane.c
+++ srcs/objects/plane.c
@@ -6,7 +6,7 @@
 /*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 13:48:39 by melshafi          #+#    #+#             */
-/*   Updated: 2024/09/12 22:39:37 by ebinjama         ###   ########.fr       */
+/*   Updated: 2024/09/13 05:53:38 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,11 @@ void plane(t_object *plane, t_double4 *center, t_mat4x4 *transform)
 t_double4	plane_normal_at(t_object *plane)
 {
 	t_double4	normal;
-	t_mat4x4	inv_transform;
+	//t_mat4x4	inv_transform;
 	t_mat4x4	inv_transpose;
 
-	inv_transform = mat4x4_inverse(&plane->transform);
-	inv_transpose = mat4x4_transpose(&inv_transform);
+	//inv_transform = mat4x4_inverse(&plane->transform);
+	inv_transpose = mat4x4_transpose(&plane->inverse);
 	normal = mat4x4_cross_vec(&inv_transpose, &plane->obj.plane.normal);
 	normal.w = 0;
 	vnormalize(&normal);
@@ -49,15 +49,15 @@ void	intersect_plane(t_ray *ray, t_object *plane, t_intersections *xs)
 	t_double4	plane_to_ray;
 	t_double4	plane_normal;
 	t_ray		trans_ray;
-	t_mat4x4	inv_transform;
+	//t_mat4x4	inv_transform;
 	double		denom;
 	double		t;
 
 	plane_normal = plane_normal_at(plane);
 	ray->itx.data->object = plane;
 	trans_ray = *ray;
-	inv_transform = mat4x4_inverse(&plane->transform);
-	ray_transform(&trans_ray, &inv_transform);
+	//inv_transform = mat4x4_inverse(&plane->transform);
+	ray_transform(&trans_ray, &plane->inverse);
 	denom = vdot(&plane_normal, &trans_ray.direction);
 	//Using fabs to get the floating point absolute number so that if denom is a small negative number,
 	//it will still be considered 0 and avoid intersecting with rays parallel to the plane
