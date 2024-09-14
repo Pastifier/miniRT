c003995f6aa20e8ed0a1500b01bf290d55fd6989
Emran Binjamaan <therealemran.design@gmail.com>
2024-09-14 21:41:02 +0400

    Unrolled d4eq

--- srcs/math/matrix4_4.c
+++ srcs/math/matrix4_4.c
@@ -65,32 +65,16 @@ bool	mat4x4_eq(t_mat4x4 *m1, t_mat4x4 *m2)
 	int	r;
 
 	r = -1;
-	while (++r < 4)
-		if (!d4eq((t_double4 *)&m1->m[r][0], (t_double4 *)&m2->m[r][0]))
-			return (false);
+	if (!d4eq((t_double4 *)&m1->m[r][0], (t_double4 *)&m2->m[r][0]))
+		return (false);
+	if (!d4eq((t_double4 *)&m1->m[r][1], (t_double4 *)&m2->m[r][1]))
+		return (false);
+	if (!d4eq((t_double4 *)&m1->m[r][2], (t_double4 *)&m2->m[r][2]))
+		return (false);
+	if (!d4eq((t_double4 *)&m1->m[r][3], (t_double4 *)&m2->m[r][3]))
+		return (false);
 	return (true);
 }
-
-//t_mat4x4	mat4x4_cross(t_mat4x4 *m1, t_mat4x4 *m2)
-//{
-//	t_mat4x4	result;
-//	int			r;
-//	int			c;
-
-//	r = -1;
-//	while (++r < 4)
-//	{
-//		c = -1;
-//		while (++c < 4)
-//		{
-//			result.m[r][c] = m1->m[r][0] * m2->m[0][c]
-//				+ m1->m[r][1] * m2->m[1][c]
-//				+ m1->m[r][2] * m2->m[2][c]
-//				+ m1->m[r][3] * m2->m[3][c];
-//		}
-//	}
-//	return (result);
-//}
 
 t_double4	mat4x4_cross_vec(t_mat4x4 *m, t_double4 *v)
 {
