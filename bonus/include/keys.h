ba41bd1274d0cf36fa1c191e1edba91eea97ac77
Emran Binjamaan <therealemran.design@gmail.com>
2024-11-06 17:04:34 +0400

    Added MacOS keysyms for space and lctrl

--- bonus/include/keys.h
+++ bonus/include/keys.h
@@ -6,7 +6,7 @@
 /*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 22:00:13 by ebinjama          #+#    #+#             */
-/*   Updated: 2024/11/05 17:31:58 by ebinjama         ###   ########.fr       */
+/*   Updated: 2024/11/06 17:02:05 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,8 @@ enum e_keysyms
 	KEY_D = 2,
 	KEY_W = 13,
 	KEY_R = 15,
+	KEY_SPACE = 49,
+	KEY_LCTRL = 256,
 	AKEY_L = 123,
 	AKEY_R,
 	AKEY_D,
