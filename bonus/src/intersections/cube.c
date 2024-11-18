9c1124942f19dfd8d4e9eb31fc273ab22e5ce0b8
Emran Binjamaan <therealemran.design@gmail.com>
2024-11-18 13:02:42 +0400

    Norminette.

--- bonus/src/intersections/cube.c
+++ bonus/src/intersections/cube.c
@@ -5,20 +5,20 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/23 11:05:59 by melshafi          #+#    #+#             */
-/*   Updated: 2024/11/08 17:20:40 by ebinjama         ###   ########.fr       */
+/*   Created: 2024/11/18 12:57:02 by ebinjama          #+#    #+#             */
+/*   Updated: 2024/11/18 13:01:40 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 #include "macros.h"
 
-static void check_axis(float origin, float dir, float side_length, t_vec4s *t)
+static void	check_axis(float origin, float dir, float side_length, t_vec4s *t)
 {
-	float half_side;
-	float tmin_numerator;
-	float tmax_numerator;
-	float temp;
+	float	half_side;
+	float	tmin_numerator;
+	float	tmax_numerator;
+	float	temp;
 
 	half_side = side_length / 2.0;
 	tmin_numerator = (-half_side - origin);
@@ -41,24 +41,28 @@ static void check_axis(float origin, float dir, float side_length, t_vec4s *t)
 	}
 }
 
+static inline void	_check_axes(t_ray *transformed_ray, t_obj *cube,
+						t_mat4s *xyz_t)
+{
+	check_axis(transformed_ray->origin.x, transformed_ray->dir.x,
+		cube->specs.side_length, &xyz_t->r1);
+	check_axis(transformed_ray->origin.y, transformed_ray->dir.y,
+		cube->specs.side_length, &xyz_t->r2);
+	check_axis(transformed_ray->origin.z, transformed_ray->dir.z,
+		cube->specs.side_length, &xyz_t->r3);
+}
+
 bool	intersect_cube(t_ray *ray, t_obj *cube, t_itx_grp *xs)
 {
-	t_ray transformed_ray;
-	t_vec4s xt;
-	t_vec4s yt;
-	t_vec4s zt;
-	t_vec4s t;
+	t_ray	transformed_ray;
+	t_mat4s	xyz_t;
+	t_vec4s	t;
 
 	transformed_ray = *ray;
 	ray_transform(&transformed_ray, &(cube->inv_transform));
-
-	check_axis(transformed_ray.origin.x, transformed_ray.dir.x, cube->specs.side_length, &xt);
-	check_axis(transformed_ray.origin.y, transformed_ray.dir.y, cube->specs.side_length, &yt);
-	check_axis(transformed_ray.origin.z, transformed_ray.dir.z, cube->specs.side_length, &zt);
-
-	t.x = fmax(fmax(xt.x, yt.x), zt.x);
-	t.y = fmin(fmin(xt.y, yt.y), zt.y);
-
+	_check_axes(&transformed_ray, cube, &xyz_t);
+	t.x = fmax(fmax(xyz_t.r1.x, xyz_t.r2.x), xyz_t.r3.x);
+	t.y = fmin(fmin(xyz_t.r1.y, xyz_t.r2.y), xyz_t.r3.y);
 	if (t.x > t.y)
 		return (false);
 	xs->arr[xs->count].object = cube;
@@ -68,16 +72,18 @@ bool	intersect_cube(t_ray *ray, t_obj *cube, t_itx_grp *xs)
 	return (true);
 }
 
-t_vec4s cube_normal_at(t_obj *cube, t_vec4s *world_point)
+t_vec4s	cube_normal_at(t_obj *cube, t_vec4s *world_point)
 {
-	t_vec4s	object_n_p[2];
-	t_vec4s	world_normal;
+	t_vec4s		object_n_p[2];
+	t_vec4s		world_normal;
+	t_vec4s		abslocal_n;
 	float		maxc;
 
 	lag_mat4s_cross_vec4s(&cube->inv_transform, world_point, &object_n_p[1]);
-
-	maxc = fmax(fmax(fabsf(object_n_p[1].x), fabsf(object_n_p[1].y)), fabsf(object_n_p[1].z));
-
+	abslocal_n.x = fabsf(object_n_p[1].x);
+	abslocal_n.y = fabsf(object_n_p[1].y);
+	abslocal_n.z = fabsf(object_n_p[1].z);
+	maxc = fmax(fmax(abslocal_n.x, abslocal_n.y), abslocal_n.z);
 	if (maxc == fabsf(object_n_p[1].x))
 		lag_vec4sv_init(&world_normal, object_n_p[1].x, 0, 0);
 	else if (maxc == fabsf(object_n_p[1].y))
