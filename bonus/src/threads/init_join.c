8342f754816d6aa58f1463f901b8da97bb1b47cd
Emran Binjamaan <therealemran.design@gmail.com>
2024-11-18 16:15:28 +0400

    Norminette.

--- bonus/src/threads/init_join.c
+++ bonus/src/threads/init_join.c
@@ -6,7 +6,7 @@
 /*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 04:47:50 by ebinjama          #+#    #+#             */
-/*   Updated: 2024/11/08 22:02:07 by ebinjama         ###   ########.fr       */
+/*   Updated: 2024/11/18 16:13:44 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@
 #include <stdio.h>
 #include <sys/time.h>
 
-extern void	*render_row(void *param); //
-//extern void	*lerp_routine(void *param); //
+extern void	*render_row(void *param);
 
 long long	my_gettime(void)
 {
@@ -28,37 +27,16 @@ long long	my_gettime(void)
 	return ((timeofday.tv_sec * 1000) + (timeofday.tv_usec / 1000));
 }
 
-void	*thread_arbiter(void *arg)
-{
-	t_thread	*data = (t_thread *)arg;
-
-	while (true)
-	{
-		pthread_mutex_lock(&data->mutex);
-		while (!data->context->stop && !data->work_ready)
-			pthread_cond_wait(&data->cond, &data->mutex);
-		if (data->context->stop)
-		{
-			pthread_mutex_unlock(&data->mutex);
-			break ;
-		}
-		pthread_mutex_unlock(&data->mutex);
-		render_row(data);
-		pthread_mutex_lock(&data->mutex);
-		data->work_ready = false;
-		pthread_mutex_unlock(&data->mutex);
-	}
-	return (NULL);
-}
-
 bool	pool_init(t_program *context)
 {
 	t_thread	*thread;
+	int			i;
 
 	context->pool = malloc(sizeof(t_thread) * _RT_NUM_THREADS);
 	if (!context->pool)
 		return (false);
-	for (int i = 0; i < _RT_NUM_THREADS; i++)
+	i = -1;
+	while (++i < _RT_NUM_THREADS)
 	{
 		thread = &context->pool[i];
 		thread->context = context;
@@ -73,11 +51,13 @@ bool	pool_init(t_program *context)
 	return (true);
 }
 
-void pool_start_frame(t_program *context)
+void	pool_start_frame(t_program *context)
 {
 	t_thread	*thread;
+	int			i;
 
-	for (int i = 0; i < _RT_NUM_THREADS; i++)
+	i = -1;
+	while (++i < _RT_NUM_THREADS)
 	{
 		thread = &context->pool[i];
 		pthread_mutex_lock(&thread->mutex);
@@ -87,18 +67,20 @@ void pool_start_frame(t_program *context)
 	}
 }
 
-void pool_wait_for_frame(t_program *context)
+void	pool_wait_for_frame(t_program *context)
 {
 	t_thread	*thread;
+	int			i;
 
-	for (int i = 0; i < _RT_NUM_THREADS; i++)
+	i = -1;
+	while (++i < _RT_NUM_THREADS)
 	{
 		thread = &context->pool[i];
 		pthread_mutex_lock(&thread->mutex);
 		while (thread->work_ready)
 		{
-			pthread_mutex_unlock(&thread->mutex); // Avoid deadlock
-			pthread_mutex_lock(&thread->mutex);	  // Re-check lock
+			pthread_mutex_unlock(&thread->mutex);
+			pthread_mutex_lock(&thread->mutex);
 		}
 		pthread_mutex_unlock(&thread->mutex);
 	}
@@ -106,39 +88,14 @@ void pool_wait_for_frame(t_program *context)
 
 bool	pool_init_join(t_program *context)
 {
-	// t_thread	*new_pool;
-	long long start_time, frame_time;
-	//int y;
+	long long	start_time;
+	long long	frame_time;
 
 	start_time = my_gettime();
-	// y = -1;
-	// context->pool = malloc(sizeof(t_thread) * _RT_NUM_THREADS);//context->pool;
-	// new_pool = context->pool;
-	// while (++y < _RT_NUM_THREADS && new_pool)
-	//{
-	//	new_pool[y].id = y;
-	//	new_pool[y].context = context;
-	//	new_pool[y].y = y * (context->cam.vsize / _RT_NUM_THREADS);
-	//	new_pool[y].y_f = (y + 1) * (context->cam.vsize / _RT_NUM_THREADS);
-	//	new_pool[y].x = y * (context->cam.hsize / _RT_NUM_THREADS);
-	//	new_pool[y].x_f = (y + 1) * (context->cam.hsize / _RT_NUM_THREADS);
-	//	pthread_create(&new_pool[y].thread, NULL, render_row, &new_pool[y]);
-	// }
-	// while (y-- && new_pool)
-	//	pthread_join(new_pool[y].thread, NULL);
-	// if (context->pool)
-	//{
-	//	mlx_put_image_to_window(context->mlx, context->win, context->canvas.ptr, 0, 0);
-	//	free(context->pool);
-	//	context->pool = NULL;
-	// }
-	//  Start work on the frame
 	pool_start_frame(context);
-	// Wait for all threads to finish
 	pool_wait_for_frame(context);
-
-	// Display the rendered frame
-	mlx_put_image_to_window(context->mlx, context->win, context->canvas.ptr, 0, 0);
+	mlx_put_image_to_window(context->mlx, context->win, context->canvas.ptr,
+		0, 0);
 	frame_time = my_gettime() - start_time;
 	printf("Frame took: %lld\n", frame_time);
 	return (true);
