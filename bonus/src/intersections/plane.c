d37ab68d87476b62d4373e3b5dd5217299acd9b7
Emran Binjamaan <therealemran.design@gmail.com>
2024-11-18 14:38:32 +0400

    Norminette.

--- bonus/src/intersections/plane.c
+++ bonus/src/intersections/plane.c
@@ -6,22 +6,12 @@
 /*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 13:48:39 by melshafi          #+#    #+#             */
-/*   Updated: 2024/11/08 17:16:11 by ebinjama         ###   ########.fr       */
+/*   Updated: 2024/11/18 14:38:17 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 #include "macros.h"
-
-t_vec4s	plane_normal_at(t_obj *plane)
-{
-	t_vec4s	local_normal;
-
-	lag_mat4s_cross_vec4s(&plane->transposed_inverse, &plane->orientation, &local_normal);
-	local_normal.w = 0;
-	lag_vec4s_normalize(&local_normal);
-	return (local_normal);
-}
 
 bool	intersect_plane(t_ray *ray, t_obj *plane, t_itx_grp *xs)
 {
@@ -29,7 +19,7 @@ bool	intersect_plane(t_ray *ray, t_obj *plane, t_itx_grp *xs)
 	float		denom;
 	float		t;
 
-	trans_ray = *ray; // BAD_MEMCPY
+	trans_ray = *ray;
 	ray_transform(&trans_ray, &plane->inv_transform);
 	if (fabsf(trans_ray.dir.y) < EPSILON)
 		return (false);
