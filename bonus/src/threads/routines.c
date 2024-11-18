394e9e69e841aa0244731684ed2f536ab56919c9
Emran Binjamaan <therealemran.design@gmail.com>
2024-11-18 15:59:19 +0400

    Norminette.

--- bonus/src/threads/routines.c
+++ bonus/src/threads/routines.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   routines.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: melshafi <melshafi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ebinjama <ebinjama@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 05:31:02 by ebinjama          #+#    #+#             */
-/*   Updated: 2024/11/07 16:32:22 by melshafi         ###   ########.fr       */
+/*   Updated: 2024/11/18 15:56:54 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,24 @@
 
 void	*render_row(void *arg)
 {
-	t_thread		*data = (t_thread *)arg;
-	t_program		*context = data->context;
-	t_camera		*cam = &context->cam;
-	int				x;
-	int				y;
-	int				y_f;
+	const t_thread		*data = (t_thread *)arg;
+	t_camera			*cam;
+	int					x;
+	int					y;
+	int					y_f;
 
 	y = data->y;
 	y_f = data->y_f;
+	cam = &data->context->cam;
 	while (y < y_f)
 	{
 		x = 0;
-		while(x < cam->hsize)
+		while (x < cam->hsize)
 		{
-			rt_render_pixel(context, x, y);
-			x+=_RT_SKIP_STEP;
+			rt_render_pixel(data->context, x, y);
+			x += _RT_SKIP_STEP;
 		}
-		y+=_RT_SKIP_STEP;
+		y += _RT_SKIP_STEP;
 	}
 	interpolate_horizontal(data);
 	interpolate_vertical(data);
