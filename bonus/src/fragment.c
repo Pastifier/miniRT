4a9a6992777bb0b648c5da12f0dec549ec1a7de3
MOMO24185 <mahmoud200012345@gmail.com>
2024-11-19 16:30:15 +0400

    Corrected Norm for fragment.c

--- bonus/src/fragment.c
+++ bonus/src/fragment.c
@@ -6,7 +6,7 @@
 /*   By: melshafi <melshafi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/27 07:07:39 by ebinjama          #+#    #+#             */
-/*   Updated: 2024/11/19 14:34:36 by melshafi         ###   ########.fr       */
+/*   Updated: 2024/11/19 16:28:21 by melshafi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@
 
 t_ray	ray_for_pixel(const t_camera *cam, int px, int py)
 {
-	t_ray		r;
-	t_vec4s		op;
-	t_vec4s		pixel;
-	t_vec4s		origin;
-	t_vec4s		direction;
+	t_ray	r;
+	t_vec4s	op;
+	t_vec4s	pixel;
+	t_vec4s	origin;
+	t_vec4s	direction;
 
 	lag_vec4sp_init(&op, (cam->half_width - (px + 0.5f) * cam->pixel_size),
-			(cam->half_height - (py + 0.5f) * cam->pixel_size), -1);
+		(cam->half_height - (py + 0.5f) * cam->pixel_size), -1);
 	lag_mat4s_cross_vec4s(&cam->inv_transform, &op, &pixel);
 	lag_vec4sp_init(&op, 0.f, 0.f, 0.f);
 	lag_mat4s_cross_vec4s(&cam->inv_transform, &op, &origin);
@@ -33,34 +33,38 @@ t_ray	ray_for_pixel(const t_camera *cam, int px, int py)
 	return (r);
 }
 
-t_itx_computation prepare_computations(t_itx *itx, t_ray *r, t_itx_grp *itxs)
+static void	prepare_comps_normal(t_itx *itx, t_itx_computation *comps)
+{
+	if (itx->object->type == SPHERE)
+		comps->normalv = sphere_normal_at(itx->object, &comps->p);
+	else if (itx->object->type == PLANE)
+		comps->normalv = plane_normal_at(itx->object, &comps->p);
+	else if (itx->object->type == CYLINDER)
+		comps->normalv = cylinder_normal_at(itx->object, &comps->p);
+	else if (itx->object->type == CUBE)
+		comps->normalv = cube_normal_at(itx->object, &comps->p);
+	else if (itx->object->type == CONE)
+		comps->normalv = cone_normal_at(itx->object, &comps->p);
+	if (lag_vec4s_dot_ret(&comps->normalv, &comps->eyev) < EPSILON)
+	{
+		comps->inside = true;
+		lag_vec4s_negate(&comps->normalv);
+	}
+	else
+		comps->inside = false;
+}
+
+t_itx_computation	prepare_computations(t_itx *itx, t_ray *r, t_itx_grp *itxs)
 {
 	t_itx_computation	comps;
-	t_vec4s			margin;
-	//float			dot;
+	t_vec4s				margin;
 
 	comps.t = itx->t;
 	comps.obj = itx->object;
 	ray_position(&comps.p, r, comps.t);
 	comps.eyev = r->dir;
 	lag_vec4s_negate_new(&comps.eyev, r->dir);
-	if (itx->object->type == SPHERE)
-		comps.normalv = sphere_normal_at(itx->object, &comps.p);
-	else if (itx->object->type == PLANE)
-		comps.normalv = plane_normal_at(itx->object, &comps.p);
-	else if (itx->object->type == CYLINDER)
-		comps.normalv = cylinder_normal_at(itx->object, &comps.p);
-	else if (itx->object->type == CUBE)
-		comps.normalv = cube_normal_at(itx->object, &comps.p);
-	else if (itx->object->type == CONE)
-		comps.normalv = cone_normal_at(itx->object, &comps.p);
-	if (lag_vec4s_dot_ret(&comps.normalv, &comps.eyev) < EPSILON)
-	{
-		comps.inside = true;
-		lag_vec4s_negate(&comps.normalv);
-	}
-	else
-		comps.inside = false;
+	prepare_comps_normal(itx, &comps);
 	lag_vec4s_scaleby(&margin, comps.normalv, EPSILON);
 	lag_vec4s_add(&comps.over_point, &comps.p, &margin);
 	lag_vec4s_sub(&comps.under_point, &comps.p, &margin);
@@ -88,13 +92,12 @@ t_color	color_at(t_world *w, t_ray *r, int depth)
 	return (shade_hit(w, &comps, depth));
 }
 
-
 t_color	rt_render_pixel(t_program *context, int x, int y)
 {
 	t_ray		r;
 	t_color		c;
 
-	r = ray_for_pixel(&context->cam, x, y); //
+	r = ray_for_pixel(&context->cam, x, y);
 	c = color_at(&context->world, &r, _RT_REFLECTION_DEPTH);
 	put_pixel(&context->canvas, x, y, &c);
 	return (c);
